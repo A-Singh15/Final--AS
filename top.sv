6af6c25acc4c7602ee6867749e314ea9544d349d
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 18:55:40 -0700

    Update top.sv

--- top.sv
+++ top.sv
@@ -4,11 +4,10 @@ module top;
 
   bit clk;
   parameter clk_per=10; //clock period
-  always #(clk_per/2) clk = ~ clk; //clock generation
+  always #(clk_per/2) clk = ~clk; //clock generation
 
-ac_if acif(clk); //Interface
-test tst(acif); //Test Program
-accumulator dut(.sum(acif.sum), .in(acif.in), .rst(acif.rst), .clk(clk)); //Design Under Test
+  ac_if acif(clk); //Interface
+  test tst(acif); //Test Program
+  accumulator dut(.sum(acif.sum), .in(acif.in), .rst(acif.rst), .clk(clk)); //Design Under Test
 
 endmodule
-
