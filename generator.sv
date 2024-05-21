8e0da19087d0ff794d1c3c6b7bd6fb951581f8fd
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 18:46:24 -0700

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -7,26 +7,26 @@ class generator;
     transaction tr;
     mailbox mbx, rtn;
 
-    extern function new(mailbox mbx, rtn);
+    extern function new(mailbox mbx, mailbox rtn);
     extern virtual task run();
     extern virtual task wrap_up();
 endclass : generator
 
-function generator::new(mailbox mbx, rtn);
+function generator::new(mailbox mbx, mailbox rtn);
     this.mbx = mbx;
     this.rtn = rtn;
 endfunction : new
 
 task generator::run();
-    while (1) begin
-        tr = new();
-        assert(tr.randomize());
-        mbx.put(tr);
-        // Wait for acknowledgment from driver
-        rtn.get(tr);
+    tr = new();
+    for (int i = 0; i < 100; i++) begin // Limiting to 100 transactions
+        tr.randomize();              // Randomize the transaction
+        mbx.put(tr);                 // Send the transaction to the driver
+        $display("Generated transaction %0d with input: %0d", i, tr.in); // Debug statement
+        rtn.get(tr);                 // Wait for acknowledgment from the driver
     end
 endtask : run
 
 task generator::wrap_up();
-    //empty for now
+    $display("Generator wrap up.");
 endtask : wrap_up
