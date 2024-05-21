9d3428ba7823ac76b789b662d3b6bfc071da8a99
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 19:12:50 -0700

    Add files via upload

--- interface.sv
+++ interface.sv
@@ -1,24 +1,21 @@
 interface ac_if (input bit clk);
-    logic [7:0] in;
-    logic rst;
-    logic [15:0] sum;
+	logic [7:0]	in;
+	logic		rst;
+	logic [15:0] sum;
 
-    clocking cb @(posedge clk);
-        default input #1 output #1;
-        output in, rst;
-        input sum;
-    endclocking : cb
+	clocking cb @(posedge clk);
+		default input #1 output #1;
+		output   in, rst;
+  		input sum;
+	endclocking : cb
 
-    modport dut (
-        input rst, in,
-        output sum
-    );
+	modport dut (
+		input		rst, in,
+		output	sum);
 
-    modport test (
-        clocking cb,
-        input clk,
-        output rst,
-        input sum // Added sum here to make it accessible in the test modport
-    );
+	modport test (
+		clocking	 cb,
+		input clk,
+		output 	 rst);
 
 endinterface : ac_if
