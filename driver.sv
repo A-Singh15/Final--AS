893e32daab4c65caf10833ef2ac1c20a620428ac
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 20:36:04 -0700

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -16,7 +16,7 @@ function driver::new(mailbox mbx, rtn, mbx_scb, input virtual ac_if.test acif);
     tr = new();
 endfunction : new
 
-task driver::run();
+/*task driver::run();
     // Apply reset
     // Note: Reset should be applied outside the driver
     @(acif.clk);
@@ -39,8 +39,31 @@ task driver::run();
         @(acif.clk);
     end
 endtask : run
+*/
+task driver::run();
+    // Apply reset
+    acif.cb.rst <= 1;
+    repeat (2) @(acif.cb);
+    acif.cb.rst <= 0;
 
+    // Check reset operation
+    if (acif.cb.sum == 0)
+        $display("Reset successful.");
+    else
+        $display("Reset failed.");
 
+    // Main driving loop
+    while (1) begin
+        mbx.get(tr);
+        // Acknowledge transaction to generator
+        rtn.put(tr);
+        // Send transaction to scoreboard
+        mbx_scb.put(tr);
+        // Drive the DUT interface
+        acif.cb.in <= tr.in;
+        @(acif.cb);
+    end
+endtask : run
 task driver::wrap_up();
     wait (acif.sum == 16'hFFFF);
     @acif.clk;
