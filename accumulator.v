b8ec6a1c46dccde140d0f4e227de1590c1cd46c2
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 20:37:26 -0700

    Update accumulator.v

--- accumulator.v
+++ accumulator.v
@@ -1,4 +1,4 @@
-module accumulator (
+/*module accumulator (
     output reg [15:0] sum,    // accumulated output
     input [7:0] in,           // input
     input rst, clk            // reset and clock inputs
@@ -12,4 +12,24 @@ begin
         sum <= sum + in;
 end
 
+endmodule
+*/
+
+`timescale 1ns/1ps
+
+module accumulator (
+    output reg [15:0] sum,  // accumulated out
+    input [7:0] in,         // input
+    input rst, clk          // reset and clock inputs
+);
+    wire carry;
+    wire [15:0] sum_in; 
+    assign {carry, sum_in} = sum + in;
+
+    always @(posedge clk) begin
+        if (rst)
+            sum <= 0;
+        else 
+            sum <= (carry) ? 16'hFFFF : sum_in;
+    end
 endmodule
