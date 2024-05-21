84a8ec62cf9bc32ad369180ea1bd095433fef72e
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 20:08:19 -0700

    Update test.sv

--- test.sv
+++ test.sv
@@ -3,17 +3,11 @@ program automatic test(ac_if.test acif);
 
 environment env;
 initial begin
-    $vcdpluson;
-    env = new(acif);
-    env.build();
-    
-    // Apply reset
-    acif.rst <= 1;
-    #20;
-    acif.rst <= 0;
-    
-    env.run();
-    env.wrap_up();
+	$vcdpluson;
+	env = new(acif);
+	env.build();
+	env.run();
+	env.wrap_up();
 end
 
 endprogram
