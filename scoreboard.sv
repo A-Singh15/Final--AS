2e6dd62b84a02964e8e00caf4ab7ef03d499c3a6
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 18:22:13 -0700

    Update scoreboard.sv
    take 2 tablets

--- scoreboard.sv
+++ scoreboard.sv
@@ -6,7 +6,7 @@ class scoreboard;
     // Functional coverage definition
     covergroup cov;
         coverpoint tr_in.in {
-            bins auto_bins = auto [16]; // 16 equally sized bins for 8-bit input data
+            bins input_bins[] = { [0:15], [16:31], [32:47], [48:63], [64:79], [80:95], [96:111], [112:127], [128:143], [144:159], [160:175], [176:191], [192:207], [208:223], [224:239], [240:255] };
         }
     endgroup
 
