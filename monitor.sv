f3bf257f0b0e135508ef79f882b9be9f292dedff
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 18:51:07 -0700

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,18 +1,18 @@
 class monitor;
-	virtual ac_if.test acif;
-	transaction tr;
-	mailbox mbx;
-	extern function new(mailbox mbx, input virtual ac_if.test acif);
-	extern virtual task run();
-	extern virtual task wrap_up();
+    virtual ac_if.test acif;
+    transaction tr;
+    mailbox mbx;
+
+    extern function new(mailbox mbx, input virtual ac_if.test acif);
+    extern virtual task run();
+    extern virtual task wrap_up();
 endclass : monitor
 
 function monitor::new(mailbox mbx, input virtual ac_if.test acif);
-	this.mbx=mbx;
-	this.acif = acif;
-	tr=new();
+    this.mbx = mbx;
+    this.acif = acif;
+    tr = new();
 endfunction : new
-
 
 task monitor::run();
     // Use a temporary variable to store rst signal value
@@ -21,28 +21,23 @@ task monitor::run();
     // Wait for reset to be applied and removed
     do begin
         @(posedge acif.clk);
-        rst_tmp = acif.rst;
+        rst_tmp = acif.cb.rst;
     end while (rst_tmp == 0);
 
     do begin
         @(posedge acif.clk);
-        rst_tmp = acif.rst;
+        rst_tmp = acif.cb.rst;
     end while (rst_tmp == 1);
 
     // Monitor DUT output in each clock cycle
     for (int i = 0; i < 100; i++) begin
         @(posedge acif.clk);
-        tr.sum = acif.sum;  // Read DUT sum output directly from the interface
-        mbx.put(tr);        // Send the transaction to the scoreboard
+        tr.sum = acif.cb.sum;  // Read DUT sum output from clocking block
+        mbx.put(tr);           // Send the transaction to the scoreboard
         $display("Captured sum: %0d", tr.sum);  // Debug statement
     end
 endtask : run
 
 task monitor::wrap_up();
-	//empty for now
+    // Empty for now
 endtask : wrap_up
-
-
-
-
-	
