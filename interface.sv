6b386f552fa285b350f856748870d8997a8ecdb2
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 18:55:27 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,22 +1,24 @@
 interface ac_if (input bit clk);
-	logic [7:0]	in;
-	logic		rst;
-	logic [15:0] sum;
+    logic [7:0] in;
+    logic rst;
+    logic [15:0] sum;
 
-	clocking cb @(posedge clk);
-		default input #1 output #1;
-		output   in, rst;
-  		input sum;
-	endclocking : cb
+    clocking cb @(posedge clk);
+        default input #1 output #1;
+        output in, rst;
+        input sum;
+    endclocking : cb
 
-	modport dut (
-		input		rst, in,
-		output	sum);
+    modport dut (
+        input rst, in,
+        output sum
+    );
 
-	modport test (
-		clocking	 cb,
-		input clk,
-		 input sum,  // Added sum here to make it accessible
-		output 	 rst);
+    modport test (
+        clocking cb,
+        input clk,
+        input sum,  // Added sum here to make it accessible
+        output rst
+    );
 
 endinterface : ac_if
