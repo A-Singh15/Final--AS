50b4e225b7fc19c539fb38bd99ed16ecb9286dcd
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 19:59:16 -0700

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -6,6 +6,7 @@ endclass
 class generator;
     transaction tr;
     mailbox mbx, rtn;
+    bit [15:0] cumulative_sum;
 
     extern function new(mailbox mbx, rtn);
     extern virtual task run();
@@ -15,12 +16,15 @@ endclass : generator
 function generator::new(mailbox mbx, rtn);
     this.mbx = mbx;
     this.rtn = rtn;
+    this.cumulative_sum = 0;
 endfunction : new
 
 task generator::run();
     while (1) begin
         tr = new();
         assert(tr.randomize());
+        tr.sum = cumulative_sum;
+        cumulative_sum = (cumulative_sum + tr.in > 16'hFFFF) ? 16'hFFFF : cumulative_sum + tr.in;
         mbx.put(tr);
         // Wait for acknowledgment from driver
         rtn.get(tr);
@@ -30,3 +34,4 @@ endtask : run
 task generator::wrap_up();
     //empty for now
 endtask : wrap_up
+
