b42b1405aa485229fc9c02a4e704163975ebd82c
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 19:24:07 -0700

    Add files via upload

---
+++ test.sv
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+program automatic test(ac_if acif);
+
+    // Declare the environment
+    environment env;
+
+    // Initial block to initialize and run the environment
+    initial begin
+        $vcdpluson;
+        env = new(acif);
+        env.build();
+        env.run();
+        env.wrap_up();
+    end
+
+endprogram : test
