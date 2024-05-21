45d78a4a66d009230beefc98af6a2913b613ad07
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 20:34:33 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,4 +1,4 @@
-interface ac_if(input logic clk, input logic rst);
+/*interface ac_if(input logic clk, input logic rst);
     logic [7:0] in;
     logic [15:0] sum;
 
@@ -8,4 +8,31 @@ interface ac_if(input logic clk, input logic rst);
         input rst, 
         input clk
     );
+endinterface : ac_if
+*/
+`timescale 1ns/1ps
+
+interface ac_if (input bit clk);
+    logic [7:0] in;
+    logic rst;
+    logic [15:0] sum;
+
+    clocking cb @(posedge clk);
+        default input #1 output #1;
+        output in, rst;
+        input sum;
+    endclocking : cb
+
+    modport dut (
+        input rst, in,
+        output sum
+    );
+
+    modport test (
+        clocking cb,
+        input clk,
+        output rst,
+        input sum
+    );
+
 endinterface : ac_if
