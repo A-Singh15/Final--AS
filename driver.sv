020a7e41701c53b689e7055ed0c670c2c7740eb2
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-20 20:50:50 -0700

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -1,3 +1,4 @@
+/*
 class driver;
     virtual ac_if.test acif;
     transaction tr;
@@ -17,6 +18,11 @@ class driver;
         repeat (2) @(acif.cb);
         acif.cb.rst <= 0;
 
+        // Check reset operation
+        if (acif.cb.sum == 0)
+            $display("Reset successful.");
+        else
+            $display("Reset failed.");
 
         // Main driving loop
         while (1) begin
@@ -28,6 +34,63 @@ class driver;
             // Drive the DUT interface
             acif.cb.in <= tr.in;
             @(acif.cb);
+        end
+    endtask : run
+
+    task wrap_up();
+        wait (acif.sum == 16'hFFFF);
+        @acif.clk;
+        $display("***Sum output saturated to 16'hFFFF; Finishing simulation***");
+        $finish;
+    endtask : wrap_up
+endclass
+*/
+
+class driver;
+    virtual ac_if.test acif;
+    transaction tr;
+    mailbox mbx, rtn, mbx_scb;
+
+    function new(mailbox mbx, rtn, mbx_scb, input virtual ac_if.test acif);
+        this.mbx = mbx;
+        this.rtn = rtn;
+        this.mbx_scb = mbx_scb;
+        this.acif = acif;
+        tr = new();
+    endfunction
+
+    task run();
+        // Apply reset
+        acif.cb.rst <= 1;
+        repeat (2) @(acif.cb);
+        acif.cb.rst <= 0;
+
+        // Check reset operation
+        if (acif.cb.sum == 0)
+            $display("Reset successful.");
+        else
+            $display("Reset failed.");
+
+        // Initial transaction to set the first DUT sum correctly
+        mbx.get(tr);
+        // Drive the DUT interface
+        acif.cb.in <= tr.in;
+        @(acif.cb);
+        // Acknowledge transaction to generator
+        rtn.put(tr);
+        // Send transaction to scoreboard
+        mbx_scb.put(tr);
+
+        // Main driving loop
+        while (1) begin
+            mbx.get(tr);
+            // Drive the DUT interface
+            acif.cb.in <= tr.in;
+            @(acif.cb);
+            // Acknowledge transaction to generator
+            rtn.put(tr);
+            // Send transaction to scoreboard
+            mbx_scb.put(tr);
         end
     endtask : run
 
