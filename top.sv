415846cb2d7585b05cb9d6342baf8a856e612463
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-21 17:44:41 -0700

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,28 +1,3 @@
-/*`timescale 1ns/1ps
-
-module top;
-
-  bit clk;
-  bit rst;
-  parameter clk_per = 10; // clock period
-  always #(clk_per/2) clk = ~clk; // clock generation
-
-  ac_if acif(clk, rst); // Interface
-  test tst(acif); // Test Program
-  accumulator dut(.sum(acif.sum), .in(acif.in), .rst(acif.rst), .clk(clk)); // Design Under Test
-
-  initial begin
-    // Apply reset
-    rst = 1;
-    #20;
-    rst = 0;
-  end
-
-endmodule
-
-
-*/
-
 `timescale 1ns/1ps
 
 module top;
